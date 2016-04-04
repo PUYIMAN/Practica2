b41b6cfeda4c71d54e7155a7f9534451a332ea6a
migueleras87 <migueleras87@gmail.com>
2016-04-04 13:27:49 +0200

    Metodos operaciones

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -6,7 +6,27 @@ public class Calculadora {
 	
 	// CONSTRUCTOR POR DEFECTO
 	public Calculadora(){
-		
+		num1=0;
+		num2=0;
+	}
+	
+	// CONSTRUCTOR PARAMETRIZADO
+	public Calculadora(double num1,double num2){
+		this.num1=num1;
+		this.num2=num2;
 	}
 
+	 // METODOS
+	public double Suma(){
+		return num1+num2;
+	}
+	public double Restar(){
+		return num1-num2;
+	}
+	public double Multiplicar(){
+		return num1*num2;
+	}
+	public double Division(){
+		return num1/num2;
+	}
 }
