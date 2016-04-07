476827c4b404e98b470ae7b53d363410d790b5db
PUYIMAN <davidpuyi777@gmail.com>
2016-04-07 12:31:39 +0200

    ea

---
+++ Calculadora/src/Principal.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		int num1,num2;
+		System.out.println("Introduce numero 1 ");
+		num1 = sc.nextInt();
+		System.out.println("Introduce numero 2 ");
+		Calculadora cal = new Calculadora();
+		System.out.println();
+	   
+	}
+
+}
